ae4d3751dcd0274978fa2476e9852fbc5842dfb5
willtop <billcuitop@gmail.com>
2019-09-05 13:34:15 -0400

    complete refactoring evaluate script and clean up the clustering script

--- Clustering_Methods/Spectral_Clustering.py
+++ Clustering_Methods/Spectral_Clustering.py
@@ -1,22 +1,16 @@
 import numpy as np
 from sklearn.cluster import KMeans
-from scipy import linalg as LA
 import matplotlib.pyplot as plt
 import sys
-sys.path.append("../Tools/")
+sys.path.append("../Utilities_Research/")
 import utils
 
-ADJ_MAT_DESIGN = 'A'
-VISUALIZE = True
-
+VISUALIZE = False
 # For now, just construct adjacency matrix layout by layout
-def compute_adj_mat_from_gains(gains_mat):
+def construct_adj_mat(gains_mat):
     number_of_links = np.shape(gains_mat)[0]
     assert np.shape(gains_mat) == (number_of_links, number_of_links)
-    if(ADJ_MAT_DESIGN=='A'):
-        adj_mat = np.maximum(gains_mat, np.transpose(gains_mat)) # no need to clear-off the diagonal
-    elif(ADJ_MAT_DESIGN=='B'):
-        adj_mat = 0
+    adj_mat = np.maximum(gains_mat, np.transpose(gains_mat)) # no need to clear-off the diagonal
     assert np.shape(adj_mat) == (number_of_links, number_of_links)
     return adj_mat
 
@@ -34,28 +28,17 @@ def visualize_layout_clusters(layout, cluster_assignments):
     plt.show()
     return
 
-
-# For now, schedule one layout at a time
-def schedule(general_para, layout, gains_mat):
+# For now, process one layout at a time
+def clustering_and_scheduling(general_para, layout, gains_mat):
     N = np.shape(gains_mat)[0]
     assert np.shape(gains_mat) == (N, N)
     assert np.shape(layout) == (N, 4)
-    adj_mat = compute_adj_mat_from_gains(gains_mat)
+    adj_mat = construct_adj_mat(gains_mat)
     assert np.shape(adj_mat) == (N, N)
     assert np.all(np.transpose(adj_mat) == adj_mat)
     laplace_mat = np.diag(np.sum(adj_mat, axis=1)) - adj_mat
     eig_vals, eig_vecs = np.linalg.eig(laplace_mat)
-    if (VISUALIZE):
-        sorted_eig_vals = np.sort(eig_vals)
-        eig_vals_diff = np.diff(sorted_eig_vals)
-        eig_vals_diff_ratios = eig_vals_diff[:-1] / eig_vals_diff[1:]
-        turning_eig_val_index = np.argmin(eig_vals_diff_ratios) + 1
-        plt.title("Eigenvalues of {}X{} Laplacian Matrix".format(N,N))
-        plt.plot(sorted_eig_vals, "*-")
-        for i in range(np.size(eig_vals)):
-            plt.annotate(i + 1, [i, sorted_eig_vals[i]])
-        plt.show()
-    # Try number of clusters one by one
+    # Linear search for optimal number of clusters
     max_sumrate = 0
     gains_diagonal = utils.get_diagonal_gains(np.expand_dims(gains_mat,axis=0))
     gains_nondiagonal = utils.get_nondiagonal_gains(np.expand_dims(gains_mat,axis=0))
@@ -63,13 +46,13 @@ def schedule(general_para, layout, gains_mat):
         smallest_eigval_indices = np.argsort(eig_vals)[1:num_of_links_on] # no need to take the zero eigen value
         eig_vecs_selected = eig_vecs[:, smallest_eigval_indices]
         km = KMeans(n_clusters=num_of_links_on, n_init=10).fit(eig_vecs_selected)
-        assignments = km.labels_
+        cluster_assignments = km.labels_
         if (VISUALIZE):
-            visualize_layout_clusters(layout, assignments)
+            visualize_layout_clusters(layout, cluster_assignments)
         allocations = np.zeros(N)
         # Select one strongest link from each cluster to schedule
         for i in range(num_of_links_on):
-            links_in_the_cluster = np.where(assignments == i)[0]
+            links_in_the_cluster = np.where(cluster_assignments == i)[0]
             strongest_link_in_the_cluster = links_in_the_cluster[np.argmax(np.diag(gains_mat)[links_in_the_cluster])]
             assert allocations[strongest_link_in_the_cluster] == 0, "having duplicate entry apperance across clusters"
             allocations[strongest_link_in_the_cluster] = 1
@@ -78,6 +61,7 @@ def schedule(general_para, layout, gains_mat):
         assert np.shape(rates)==(1,N)
         sumrate = np.sum(rates)
         if(sumrate > max_sumrate):
+            best_cluster_assignments = cluster_assignments
             best_allocations = allocations
             max_sumrate = sumrate
-    return best_allocations
+    return best_cluster_assignments, best_allocations
